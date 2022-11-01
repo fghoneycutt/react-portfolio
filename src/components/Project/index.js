3bd29fa3d18c1bc515d1357e1d8a803852a2bb27
fghoneycutt <fghoneycutt@gmail.com>
2022-10-31 22:14:27 -0400

    refactored project section

--- src/components/Project/index.js
+++ src/components/Project/index.js
@@ -1,112 +1,38 @@
 import React from "react";
-
+import {projects} from '../../projects'
 function Project() {
   return (
     <div className="image-grid">
-      <div className="portfolio-image-container">
-        <img
-          className="portfolio-img"
-          src="./assets/images/genre.jpeg"
-          alt="genre buddy landing page"
-        ></img>
-        <div className="portfolio-links">
-          <a
-            href="https://fghoneycutt.github.io/genre-buddy/"
-            target="_blank"
-            rel="noreferrer"
-          >
-            <span>Genre Buddy</span>
-          </a>
-          <a
-            href="https://github.com/fghoneycutt/genre-buddy"
-            target="_blank"
-            rel="noreferrer"
-          >
+      {projects.map((data) => {
+        return (
+          <div className="portfolio-image-container">
             <img
-              src="./assets/images/github.svg"
-              alt="genre buddy github link"
+              className="portfolio-img"
+              src={"./assets/images/" + data.image}
+              alt={ data.alt + " landing page"}
             ></img>
-          </a>
-        </div>
-      </div>
-      <div className="portfolio-image-container">
-        <img
-          className="portfolio-img"
-          src="./assets/images/vinder.png"
-          alt="vinder landing page"
-        ></img>
-        <div className="portfolio-links">
-          <a
-            href="https://lonely-gamer-hours.herokuapp.com/"
-            target="_blank"
-            rel="noreferrer"
-          >
-            <span>Vinder</span>
-          </a>
-          <a
-            href="https://github.com/farrelldianni/Fullstack-Group-Project"
-            target="_blank"
-            rel="noreferrer"
-          >
-            <img
-              src="./assets/images/github.svg"
-              alt="vinder github link"
-            ></img>
-          </a>
-        </div>
-      </div>
-      <div className="portfolio-image-container">
-        <img
-          className="portfolio-img"
-          src="./assets/images/tech-blog.png"
-          alt="genre buddy landing page"
-        ></img>
-        <div className="portfolio-links">
-          <a
-            href="https://griffin-tech-blog.herokuapp.com/"
-            target="_blank"
-            rel="noreferrer"
-          >
-            <span>Tech Blog</span>
-          </a>
-          <a
-            href="https://github.com/fghoneycutt/tech-blog"
-            target="_blank"
-            rel="noreferrer"
-          >
-            <img
-              src="./assets/images/github.svg"
-              alt="tech blog github link"
-            ></img>
-          </a>
-        </div>
-      </div>
-      <div className="portfolio-image-container">
-        <img
-          className="portfolio-img"
-          src="./assets/images/countygame.png"
-          alt="county game page"
-        ></img>
-        <div className="portfolio-links">
-          <a
-            href="https://fghoneycutt.github.io/county-game/"
-            target="_blank"
-            rel="noreferrer"
-          >
-            <span>North Carolina County Game</span>
-          </a>
-          <a
-            href="https://github.com/fghoneycutt/county-game"
-            target="_blank"
-            rel="noreferrer"
-          >
-            <img
-              src="./assets/images/github.svg"
-              alt="county game github link"
-            ></img>
-          </a>
-        </div>
-      </div>
+            <div className="portfolio-links">
+              <a
+                href={data.deployedLink}
+                target="_blank"
+                rel="noreferrer"
+              >
+                <span>{data.title}</span>
+              </a>
+              <a
+                href={data.ghLink}
+                target="_blank"
+                rel="noreferrer"
+              >
+                <img
+                  src="./assets/images/github.svg"
+                  alt={ data.alt + " github link"}
+                ></img>
+              </a>
+            </div>
+          </div>
+        );
+    })}
     </div>
   );
 }
---
+++ src/projects.js
@@ -0,0 +1,30 @@
+export const projects = [
+  {
+    title: "Genre Buddy",
+    image: "genre.jpeg",
+    alt: "genre buddy",
+    deployedLink: "https://fghoneycutt.github.io/genre-buddy/",
+    ghLink: "https://github.com/fghoneycutt/genre-buddy",
+  },
+  {
+    title: "Vinder",
+    image: "vinder.png",
+    alt: "vinder",
+    deployedLink: "https://lonely-gamer-hours.herokuapp.com/",
+    ghLink: "https://github.com/farrelldianni/Fullstack-Group-Project",
+  },
+  {
+    title: "Tech Blog",
+    image: "tech-blog.png",
+    alt: "tech blog",
+    deployedLink: "https://griffin-tech-blog.herokuapp.com/",
+    ghLink: "https://github.com/fghoneycutt/tech-blog",
+  },
+  {
+    title: "County Game",
+    image: "countygame.png",
+    alt: "county game",
+    deployedLink: "https://fghoneycutt.github.io/county-game/",
+    ghLink: "https://github.com/fghoneycutt/county-game",
+  },
+];
